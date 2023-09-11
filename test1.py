fd1fff7e7b263429aa069e518e358be4704f7887
RaresPascale <98171722+RaresPascale@users.noreply.github.com>
2023-09-11 15:59:15 +0300

    test1.py

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ from tkinter import filedialog as fd
 from tkinter import ttk  # Import Treeview from ttk module
 
 
-# Function to open and display the data in the text file
+# Function to open the data from the text file
 def open_txt():
     global file_path
     file_path = fd.askopenfilename(filetypes=[("Text Files", "*.txt")])
@@ -15,7 +15,7 @@ def open_txt():
         stuff = text_file.read()
         my_text.insert(END, stuff)
 
-
+# Function to display the data from the last line, in the table
 def display_table():
     table.delete(*table.get_children())
 
@@ -36,7 +36,7 @@ def display_table():
         table_row = [str(i + 1), prod_value, date, time]
         table.insert("", "end", values=table_row,)
 
-
+# Function which prints the data from the last line in a sorted way
 def sort_table():
     table.delete(*table.get_children())
 
@@ -60,27 +60,27 @@ def sort_table():
     f.close()
 
 
-
+# Function for removing the file
 def remove_file():
     my_text.delete(1.0, END)  # Clear the text widget
 
-
+# Function that clears the table
 def clear_table():
     table.delete(*table.get_children())
 
-#Root
-
+# Root
 root = Tk()
 root.title('Lista ta de cumparaturi')
 root.geometry("1000x600")
 
-# setting attribute
+# Setting attribute
 width= root.winfo_screenwidth()
 height= root.winfo_screenheight()
-#setting tkinter window size
+
+# Setting tkinter window size
 root.geometry("%dx%d" % (width, height))
 
-#Style
+# Style
 style = ttk.Style()
 style.theme_use("clam")
 style.configure("Treeview",
@@ -91,7 +91,7 @@ style.configure("Treeview",
 style.map('Treeview',
                 background=[('selected', '#653a27')])
 
-# Add image file
+# Adding image file
 bg = PhotoImage(file="Images/Supermarket5.png")
 
 #Label for image
@@ -100,28 +100,21 @@ my_label = Label(root, image=bg)
 my_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-#Titlul aplicatiei
 
-#my_text = Label(root, text="Lista de Cumparaturi",font=("Helvetica", 40), bg="#c46b46")
-#my_text.pack(pady=100)
-
-
-#Frame-ul pentru tabel
-
+# Table frame
 table_frame = Frame(root)
 table_frame.pack(pady=200)
 
 
-# Treeview widget inside the frame
-
+# Treeview widget 
 table = ttk.Treeview(table_frame, columns=("Nr.crt.", "Prod", "Data", "Ora"), show="headings")
 table.heading("Nr.crt.", text="Nr.crt.",anchor=CENTER)
 table.heading("Prod", text="Prod",anchor=CENTER)
 table.heading("Data", text="Data",anchor=CENTER)
 table.heading("Ora", text="Ora",anchor=CENTER)
 
-#Table columns
 
+#Table columns
 table.pack()
 
 my_text = Text(root, width=40, height=10, font=("Helvetica", 18), bg="#c46b46")
@@ -129,11 +122,9 @@ my_text.pack(pady=20)
 
 
 # Buttons area
-
 open_button = Button(root, text='Import', font=("Helvetica", 12), height=2, width=20, bg="#1e81b0", command=open_txt)
 open_button.pack(pady=30)
 open_button.place(x=80, y=250)
-
 
 display_button = Button(root, text='Export', font=("Helvetica", 12), height=2, width=20, bg="#1e81b0", command=display_table)
 display_button.pack(pady=30)
@@ -143,11 +134,9 @@ sort_button = Button(root, text='Sort Table', font=("Helvetica", 12), height=2, 
 sort_button.pack(pady=30)
 sort_button.place(x=80, y=450)
 
-
 remove_button = Button(root, text='Remove File', font=("Helvetica", 12), height=2, width=20, bg="#1e81b0", command=remove_file)
 remove_button.pack(pady=30)
 remove_button.place(x=1650, y=250)
-
 
 clear_button = Button(root, text="Clear Table", font=("Helvetica", 12), height=2, width=20, bg="#1e81b0", command=clear_table)
 clear_button.pack(pady=30)
